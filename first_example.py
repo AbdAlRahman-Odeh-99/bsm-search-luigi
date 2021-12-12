ff389d74c5e064ac482fc0d76197082125bf4e54
AbdAlRahman-Odeh-99 <abdalrahmanodeh1@gmail.com>
2021-12-12 04:45:11 +0300

    mc_merge_root_after_generate

--- first_example.py
+++ first_example.py
@@ -1,3 +1,4 @@
+from re import M
 import luigi
 import subprocess
 import luigi.tools.deps_tree as deps_tree
@@ -21,7 +22,7 @@ def generatePrepareCommand():
         rm -rf {base_dir}
         mkdir -p {base_dir}
         """
-
+#----------------------------------- Generate Operation Start -----------------------------------
 def generate_data_generation(type, nevents, job_number):
     output_file =  base_dir + "/" + type + "_" + str(job_number) + ".root"
     return {'type':type,'nevents':nevents,'jobnumber':str(job_number), 'output_file':output_file}
@@ -34,6 +35,27 @@ def generate_GenerateCommand(type, nevents, jobnumber, output_file):
         python {code_dir}/generatetuple.py {type} {nevents} {output_file}
     """
 
+class Generate(luigi.Task):
+    task_namespace = 'bsm-search'
+    data = luigi.DictParameter()
+
+    def requires(self):
+        return Scatter(self.data['type'])
+    def output(self):
+        output_file = self.data['output_file']
+        return luigi.LocalTarget(output_file)
+    def run(self):
+        type = self.data['type']
+        jobnumber = self.data['jobnumber']
+        nevents = self.data['nevents']
+        output_file = self.data['output_file']
+        bashCommand = generate_GenerateCommand(type, nevents, jobnumber, output_file)
+        process = subprocess.Popen(bashCommand, shell = True, executable='/bin/bash',stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        output, error = process.communicate()
+        print("The command is: \n",bashCommand)
+        print("The output is: \n",output.decode())
+        print("The error is: \n",error.decode())
+#----------------------------------- Generate Operation End -----------------------------------
 class PrepareDirectory(luigi.Task):
     task_namespace = 'bsm-search'
 
@@ -73,36 +95,66 @@ class Scatter(luigi.Task):
         with open(output_file,'w') as outfile:
             json.dump(json_object,outfile)
 
-class Generate(luigi.Task):
+#----------------------------------- Merge Root Operation Start -----------------------------------
+def merge_root_data_generation(type, njobs):
+    output_file =  base_dir + "/" + type + ".root"
+    return {'type':type,'njobs':njobs, 'output_file':output_file}
+
+def merge_root_GenerateCommand(type, njobs):
+    return f"""
+        set -x
+        BASE_DIR={base_dir}
+        BASE={type}
+        END={njobs}
+        INPUTS=''
+        OUTPUT="$BASE_DIR/$BASE"
+        echo $INPUTS
+        echo Output: $OUTPUT
+        C=1   
+        while [ $C -le $((END)) ]; do INPUTS="$INPUTS $OUTPUT""_$C.root"; C=$((C+1)); done
+        echo Inputs: $INPUTS
+        source {thisroot_dir}/thisroot.sh
+        hadd -f $OUTPUT.root $INPUTS
+        """
+    #while [ $C -le $((END)) ]; do INPUTS="$INPUTS $BASE_DIR/$BASE_$C.root"; ((C++)); done
+    #while [ $C -le $((END)) ]; do INPUTS="$INPUTS $BASE_DIR/$BASE_$C.root"; $((C++)); done
+class Merge_Root(luigi.Task):
     task_namespace = 'bsm-search'
     data = luigi.DictParameter()
 
     def requires(self):
-        return Scatter(self.data['type'])
-    
+        generate_list = []
+        type = self.data['type']
+        njobs = mc_options[type]['njobs']
+        nevents = mc_options[type]['nevents']
+        for i in range (0, njobs):
+            data = generate_data_generation(type, nevents, i+1)
+            generate_list.append(Generate(data))
+        return generate_list
+
     def output(self):
         output_file = self.data['output_file']
         return luigi.LocalTarget(output_file)
     
     def run(self):
         type = self.data['type']
-        jobnumber = self.data['jobnumber']
-        nevents = self.data['nevents']
-        output_file = self.data['output_file']
-        bashCommand = generate_GenerateCommand(type, nevents, jobnumber, output_file)
-        process = subprocess.Popen(bashCommand, shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        njobs = self.data['njobs']
+        bashCommand = merge_root_GenerateCommand(type, njobs)
+        process = subprocess.Popen(bashCommand, shell = True, executable='/bin/bash',stdout = subprocess.PIPE, stderr = subprocess.PIPE)
         output, error = process.communicate()
         print("The command is: \n",bashCommand)
         print("The output is: \n",output.decode())
         print("The error is: \n",error.decode())
 
-          
+#----------------------------------- Merge Root Operation End -----------------------------------
 
 if __name__ == '__main__':
     #luigi.run(['bsm-search.PrepareDirectory', '--workers', '1', '--local-scheduler'])
     #luigi.run(['bsm-search.Scatter', '--workers', '1', '--local-scheduler'])
     #luigi.build(list_of_tasks, workers=1, local_scheduler=True)
     
+
+    '''
     for key in mc_options.keys():
         njobs = mc_options[key]['njobs']
         nevents = mc_options[key]['nevents']
@@ -110,6 +162,12 @@ if __name__ == '__main__':
             data = generate_data_generation(key, nevents, i+1)
             list_of_tasks.append(Generate(data))
             #print(deps_tree.print_tree(Generate(data)))
+    '''
+    for key in mc_options.keys():
+        njobs = mc_options[key]['njobs']
+        data = merge_root_data_generation(key, njobs)
+        list_of_tasks.append(Merge_Root(data))
+
     luigi.build(list_of_tasks, workers = 4)
     
     #luigi.build(AllTasks)
